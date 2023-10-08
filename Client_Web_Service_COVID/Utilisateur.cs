6381cb2e5e63e7a55040f8dfc951f9ab3a7d056e
Mamadou <senemamadou1999@gmail.com>
2023-10-08 15:45:27 +0000

    ajout des proprites JsonProperty pour mapper les proprites de la classe avec les données json

--- Client_Web_Service_COVID/Utilisateur.cs
+++ Client_Web_Service_COVID/Utilisateur.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Client_Web_Service_COVID
 {
-    internal class Utilisateur
+    [JsonObject]
+    [XmlRoot("utilisateurs")]
+    public class Utilisateurs
     {
+        [XmlElement("utilisateur")]
+        public List<Utilisateur> ListeUtilisateurs { get; set; }
+    }
+    [XmlRoot("utilisateur")]
+    [JsonObject]
+    public class Utilisateur
+    {
+        [XmlElement("idUtilisateur")]
+        [JsonProperty("idUtilisateur")]
+        public int Id { get; set; }
+        [XmlElement("prenom")]
+        [JsonProperty("prenom")]
         public string Prenom { get;set; }
-        public string Nom { get;set; }  
-        public string Email { get; set;}   
+        [XmlElement("nom")]
+        [JsonProperty("nom")]
+        public string Nom { get;set; }
+        [XmlElement("email")]
+        [JsonProperty("email")]
+        public string Email { get; set;}
+        [XmlElement("phone")]
+        [JsonProperty("phone")]
         public string Telephone { get;set; }
+        [XmlElement("password")]
+        [JsonProperty("password")]
         public string Password { get;set; }
     }
 }
