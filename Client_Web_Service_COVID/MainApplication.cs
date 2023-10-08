e8daa76cccae8b170ed536b297fe1ea0f2da3093
Mamadou <senemamadou1999@gmail.com>
2023-10-08 15:48:31 +0000

    definition des methodes pour le chargement des structures de santés

--- Client_Web_Service_COVID/MainApplication.cs
+++ Client_Web_Service_COVID/MainApplication.cs
@@ -9,12 +9,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
+using System.Net.Http.Headers;
+using System.IO;
 
 namespace Client_Web_Service_COVID
 {
     public partial class MainApplication : Form
     {
-        private List<Utilisateur> list;
         public MainApplication()
         {
             InitializeComponent();
@@ -22,47 +23,100 @@ namespace Client_Web_Service_COVID
 
         private void MainApplication_Load(object sender, EventArgs e)
         {
-            Console.WriteLine("Chargement des info ...");
-            //Chargement();
+            Chargement();
         }
         public async void Chargement()
         {
-            try
-            {
-                list=new List<Utilisateur>();
-                HttpClient client = new HttpClient();
-                String url = "http://localhost:8080/REST_COVID_WEB_SERVICE/rs/utilisateurs";
-                HttpResponseMessage responseMessage = await client.GetAsync(url);
-                if(responseMessage.IsSuccessStatusCode) 
+            using(HttpClient client = new HttpClient()) 
+            { 
+                try
                 {
-                    String xmlResponse=await responseMessage.Content.ReadAsStringAsync();
-                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(Utilisateur));
-                    using (var stringReader = new System.IO.StringReader(xmlResponse))
+                    String url = "http://localhost:8080/REST_COVID_WEB_SERVICE/rs/structures";
+                    //effectuer une requête vers l'URL du serveur
+                    HttpRequestMessage request =new HttpRequestMessage(HttpMethod.Get, url);
+                    request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                    HttpResponseMessage response = await client.SendAsync(request);
+                    if(response.IsSuccessStatusCode) 
                     {
-                        var utilisateur = (Utilisateur)xmlSerializer.Deserialize(stringReader);
-                        list.Add(utilisateur);
-                        foreach (var u in list)
+
+                        //lecture du contenu de la reponse
+                        String xmlResponse = await response.Content.ReadAsStringAsync();
+                        // Désérialisez la réponse XML en objets C#
+                        using (var stringReader = new StringReader(xmlResponse))
                         {
-                            Console.WriteLine($"Nom: {u.Nom}, Prénom: {u.Prenom}, Email: {u.Email}");
+                            XmlSerializer serializer=new XmlSerializer(typeof(Structuredesantes));
+                            Structuredesantes structuredesantes = (Structuredesantes)serializer.Deserialize(stringReader);
+                            /*foreach (Utilisateur u in utilisateurs.ListeUtilisateurs)
+                             {
+                                 Console.WriteLine($"Nom: {u.Nom}, Prénom: {u.Prenom}, Email: {u.Email}");
+                             }*/
+                            this.dataGridView1.DataSource = structuredesantes.ListStructures;
                         }
                     }
-                }
-                else
-                {
-                    Console.WriteLine($"La requête a échoué avec le code {responseMessage.StatusCode}");
-                }
+                    else
+                    {
+                        Console.WriteLine($"La requête a échoué avec le code {response.StatusCode}");
+                    }
 
+                }
+                catch(Exception ex) 
+                {
+                    MessageBox.Show("Une erreur s'est produite : " + ex.Message);
+                }
             }
-            catch(Exception ex) 
-            {
-                MessageBox.Show("Une erreur s'est produite : " + ex.Message);
-            }
-        
+
         }
 
-        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private async void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
         {
+            if(e.RowIndex>=0 && e.RowIndex<dataGridView1.Rows.Count) 
+            {
+                int idStructuresante = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["idStructuresante"].Value);
+                Console.WriteLine(idStructuresante);
+                using(HttpClient client = new HttpClient()) 
+                {
+                    try
+                    {
+                        string url = $"http://localhost:8080/REST_COVID_WEB_SERVICE/rs/structures/{idStructuresante}";
+                        HttpRequestMessage request=new HttpRequestMessage(HttpMethod.Get, url);
+                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+                        HttpResponseMessage response = await client.SendAsync(request);
+                        if(response.IsSuccessStatusCode)
+                        {
+                            string xmlResponse =await response.Content.ReadAsStringAsync();
+                            using(StringReader stringReader = new StringReader(xmlResponse)) 
+                            {
+                                XmlSerializer serialiser=new XmlSerializer(typeof(Structuredesante));
+                                Structuredesante sante =(Structuredesante) serialiser.Deserialize(stringReader);
+                                List<dynamic> list = new List<dynamic>();
+                                list.Add(sante);
+                                this.dataGridView1.DataSource = list;
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Erreur ! la requete n'a pas aboutie");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Erreur survenue : " + ex.Message);
+                    }
+                }
 
+            }
+        }
+
+        private void nosStructuresDeSantéToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Chargement();
+        }
+
+        private void deconnexionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Form1 form = new Form1();
+            form.Show();
         }
     }
 }
