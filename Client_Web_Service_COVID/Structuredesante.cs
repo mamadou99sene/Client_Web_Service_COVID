30d81d9f0a6bee3fbf4c2fc0d5ad665e2236097c
Mamadou <senemamadou1999@gmail.com>
2023-10-10 23:12:31 +0000

    ajout des annotations json

--- Client_Web_Service_COVID/Structuredesante.cs
+++ Client_Web_Service_COVID/Structuredesante.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,25 +9,34 @@ using System.Xml.Serialization;
 namespace Client_Web_Service_COVID
 {
     [XmlRoot("structuredesantes")]
+    [JsonObject]
     public class Structuredesantes
     {
         [XmlElement("structuredesante")]
+        [JsonProperty("structuredesante")]
         public List<Structuredesante> ListStructures { get; set; }
     }
     [XmlRoot("structuredesante")]
+    [JsonObject]
     public class Structuredesante
     {
             [XmlElement("idStructuresante")]
+        [JsonProperty("idStructuresante")]
         public int IdStructuresante { get;set;}
         [XmlElement("capacite")]
+        [JsonProperty("capacite")]
         public int Capacite { get; set; }
         [XmlElement("localisation")]
+        [JsonProperty("localisation")]
         public string Localisation { get;set; }
             [XmlElement("telephone")]
+        [JsonProperty("telephone")]
         public string Telephone { get;set; }
             [XmlElement("typeTraitement")]
+        [JsonProperty("typeTraitement")]
         public string TypeTraitement { get;set; }
             [XmlElement("typetest")]
+        [JsonProperty("typetest")]
         public string Typetest { get;set; }
     }
 }
