4d569fbd6785e53c01814110cfcea4a93d676ba3
Mamadou <senemamadou1999@gmail.com>
2023-10-12 14:11:31 +0000

    definition du code pour la consultation des rendez vous

--- Client_Web_Service_COVID/ConsultationRendezVous.Designer.cs
+++ Client_Web_Service_COVID/ConsultationRendezVous.Designer.cs
@@ -38,9 +38,9 @@
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
             this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(85, 145);
+            this.dataGridView1.Location = new System.Drawing.Point(2, 145);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(614, 212);
+            this.dataGridView1.Size = new System.Drawing.Size(796, 212);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
--- Client_Web_Service_COVID/ConsultationRendezVous.cs
+++ Client_Web_Service_COVID/ConsultationRendezVous.cs
@@ -24,11 +24,27 @@ namespace Client_Web_Service_COVID
         private async void ConsultationRendezVous_Load(object sender, EventArgs e)
         {
             RendezVs rendezV = await LoadRendezVous();
-            this.dataGridView1.DataSource = rendezV.ListRendezVous;
+            List<RepresentationRv> list_rv = new List<RepresentationRv>(rendezV.ListRendezVous.Count);
+            RepresentationRv representation=null;
+            for (int i = 0; i < rendezV.ListRendezVous.Count; i++)
+            {
+                representation = new RepresentationRv();
+                representation.Id = rendezV.ListRendezVous[i].Id;
+                representation.Date = rendezV.ListRendezVous[i].Date;
+                representation.Type = rendezV.ListRendezVous[i].Type;
+                representation.StatusRv = rendezV.ListRendezVous[i].Status;
+                representation.UserFirstName = rendezV.ListRendezVous[i].User.Prenom;
+                representation.UserLastName = rendezV.ListRendezVous[i].User.Nom;
+                representation.UserEmail = rendezV.ListRendezVous[i].User.Email;
+                representation.StructureLocate = rendezV.ListRendezVous[i].Structuredesante.Localisation;
+                representation.PhoneStructure = rendezV.ListRendezVous[i].Structuredesante.Telephone;
+                list_rv.Add(representation);
+            }
+            this.dataGridView1.DataSource =list_rv;
         }
         public async Task<RendezVs> LoadRendezVous()
         {
-            RendezVs AllRv = null;
+            RendezVs AllRv = null;  
             using (HttpClient client = new HttpClient())
             {
                 try
