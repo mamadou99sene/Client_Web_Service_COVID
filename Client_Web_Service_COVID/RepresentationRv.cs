a932668f0cdf2ca0731ec8c581f87a7cdd1074db
Mamadou <senemamadou1999@gmail.com>
2023-10-12 14:19:08 +0000

    ajout de la classe pour la representation de l'affichage des rendez vous

---
+++ Client_Web_Service_COVID/RepresentationRv.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_Web_Service_COVID
+{
+    internal class RepresentationRv
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public string StructureLocate { get; set; }
+        public string StatusRv { get; set; }    
+        public string PhoneStructure { get; set; }
+    }
+}
